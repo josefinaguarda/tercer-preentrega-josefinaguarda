3658f826d8d0db5d23f52d1748331032ca99d8de
josefinaguarda <josefinaguarda66@gmail.com>
2024-05-21 15:13:16 -0300

    cambio en settings de config

--- mi_proyecto/config/settings.py
+++ mi_proyecto/config/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mi_aplicacion',
     'core',
+    'mi_aplicacion',
 ]
 
 MIDDLEWARE = [
