b5f180b9c76953c3cb45770a6c108b60a4658deb
josefinaguarda <josefinaguarda66@gmail.com>
2024-05-23 23:01:23 -0300

    modifique models.py

--- mi_proyecto/mi_aplicacion/models.py
+++ mi_proyecto/mi_aplicacion/models.py
@@ -13,9 +13,14 @@ class Refugio(models.Model):
     nombre = models.CharField(max_length=150)
     direccion = models.CharField(max_length=200)
     telefono = models.CharField(max_length=20)
-
+    mascota_id= models.ManyToManyField(Mascota, related_name='refugios', blank=True)
+    
+    def __str__(self):
+        return self.nombre
+    
 class Adoptante(models.Model):
     nombre = models.CharField(max_length=150)
     direccion = models.CharField(max_length=200)
     telefono = models.CharField(max_length=20)
     mascota_id= models.ForeignKey(Mascota, on_delete=models.SET_NULL, null=True, blank=True)
+    
